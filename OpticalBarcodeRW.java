5671bccc6c9fab02ebcbe0a5c0ec49c44c690244
CristianVazquez2020 <62410281+CristianVazquez2020@users.noreply.github.com>
2020-03-27 17:50:31 -0700

    phase 1 complete?
    Created the BarcodeIO interface and implemented Cloneable in BarcodeImage

---
+++ OpticalBarcodeRW.java
@@ -0,0 +1,62 @@
+public class OpticalBarcodeRW
+{
+   public static void main( String [] args)
+   {     
+      // Clone Test
+      BarcodeImage barcode1 = new BarcodeImage();
+      BarcodeImage barcode2 = new BarcodeImage();
+      
+      barcode2 = (BarcodeImage) barcode1.clone();
+      
+      if (barcode1 == barcode2)
+      {
+         System.out.println("Clone was unsuccessful");
+      }
+      else
+      {
+         System.out.println("Clone was successful");
+      }
+   }
+}
+
+interface BarcodeIO
+{
+   public boolean scan(BarcodeImage bc);
+   public boolean readText(String text);
+   public boolean generateImageFromText();
+   public boolean translateImageToText();
+   public void displayTextToConsole();
+   public void displayImageToConsole();
+
+}
+
+class BarcodeImage implements Cloneable
+{
+   public static final int MAX_HEIGHT = 30;
+   public static final int MAX_WIDTH = 65;
+   private boolean[][] imageData;
+  
+   public BarcodeImage()
+   {
+      imageData = new boolean[65][30];
+   }
+   
+   public Object clone()
+   {
+      try
+      {
+         BarcodeImage copy = (BarcodeImage) super.clone();
+         return copy;
+      }
+      catch (CloneNotSupportedException e)
+      {
+         return null;
+      }
+   }
+}
+
+/**class DataMatrix implements BarcodeIO
+{
+   
+}**/
+
